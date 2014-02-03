64a6cfbf3386c92b96283f6c9ec134befb338c60
Mike Haas <mikehaas763@gmail.com>
2014-02-03 01:27:28 -0600

    Add license and copyright header to files. Rename package.

---
+++ src/resource.js
@@ -0,0 +1,9 @@
+/*
+ * This file is part of the hateoasjs package.
+ *
+ * (c) Mike Haas <mike@emken.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
